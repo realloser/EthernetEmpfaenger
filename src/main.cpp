599dce96516c49984ca152acd7bdf7544969ea27
Martyn Messerli <realloser@gmx.ch>
2018-08-20 20:56:36 +0200

    Send data should be a different repository

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,6 @@
 #include <my_dht.h>
 #include <light_resistor.h>
 #include <bmp280.h>
-#include <send_data.h>
 #include <receive_data.h>
 #include <delayAsync.h>
 #include <ethernetSender.h>
@@ -25,15 +24,8 @@ void setup()
   setupDHT();
   inputVoltage = 503; // 423 volt on the nano on the breadboard
   setupLightIntesnity();
-  if (RECIVER)
-  {
-    setupReceiveData();
-  }
-  else
-  {
-    setupSendData();
-    sendPing();
-  }
+  setupReceiveData();
+
 
   readAllSensors();
 
@@ -51,19 +43,7 @@ void readLoop()
 void loop()
 {
   readLoop();
-
-  if (RECIVER)
-  {
-    loopReceiveData();
-  }
-  else
-  {
-    // multiply the readings by factor 100 so we can just send the int.
-    // node name, message index, primary temp, humidity, light intensity, voltage if any, secondary temp, air pressure
-    sprintf(transmissionMessage, "%s|%i|%i|%i|%i|%i|%i|%lu", NODE_HASH, messageIndex, (int)(dhtTemp * 100), (int)(dhtHum * 100), lightIntensity, -1, (int)(bmpTemperatur * 100), (unsigned long)(bmpPressure * 100));
-    sendData();
-  }
-
+  loopReceiveData();
   loopEthernet();
 }
 
