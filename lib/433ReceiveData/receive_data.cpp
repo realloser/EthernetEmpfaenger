ab6530f171d508133e2d7b5d43f15077dbf69c0d
Martyn Messerli <realloser@gmx.ch>
2018-08-24 01:47:34 +0200

    Cleanup

--- lib/433ReceiveData/receive_data.cpp
+++ lib/433ReceiveData/receive_data.cpp
@@ -10,45 +10,45 @@ uint8_t receivedMessage[VW_MAX_MESSAGE_LEN];
 
 void setupReceiveData()
 {
-    delay(1000);
+  delay(1000);
 
-    // Initialise the IO and ISR
-    vw_set_rx_pin(receive_pin);
-    vw_set_ptt_inverted(true); // Required for DR3100
-    vw_setup(2000);	 // Bits per sec
+  // Initialise the IO and ISR
+  vw_set_rx_pin(receive_pin);
+  vw_set_ptt_inverted(true); // Required for DR3100
+  vw_setup(2000);            // Bits per sec
 
-    vw_rx_start();       // Start the receiver PLL running
+  vw_rx_start(); // Start the receiver PLL running
 }
 
-void loopReceiveData()
+bool loopReceiveData()
 {
-   
+  if (vw_get_message(receivedMessage, &buflen)) // Non-blocking
+  {
+    int i;
 
-    if (vw_get_message(receivedMessage, &buflen)) // Non-blocking
+    digitalWrite(led_pin, HIGH); // Flash a light to show received good message
+    // Message with a good checksum received, print it.
+    Serial.print("Got: ");
+
+    for (i = 0; i < buflen; i++)
     {
-      	int i;
-
-        digitalWrite(led_pin, HIGH); // Flash a light to show received good message
-      	// Message with a good checksum received, print it.
-      	Serial.print("Got: ");
-
-      	for (i = 0; i < buflen; i++)
-      	{
-      	    Serial.print(receivedMessage[i], HEX);
-      	    Serial.print(' ');
-      	}
-      	Serial.println();
-
-        for (i = 0; i < buflen; i++)
-      	{
-           if (receivedMessage[i] == '\0')
-           {
-             break;
-           }
-      	   Serial.print((char)receivedMessage[i]);
-      	}
-      	Serial.println();
-
-        digitalWrite(led_pin, LOW);
+      Serial.print(receivedMessage[i], HEX);
+      Serial.print(' ');
     }
+    Serial.println();
+
+    for (i = 0; i < buflen; i++)
+    {
+      if (receivedMessage[i] == '\0')
+      {
+        break;
+      }
+      Serial.print((char)receivedMessage[i]);
+    }
+    Serial.println();
+
+    digitalWrite(led_pin, LOW);
+    return true;
+  }
+  return false;
 }
