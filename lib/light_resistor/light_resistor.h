fce84baee113dcf2ad772f0636adbab280830a4c
Martyn Messerli <realloser@gmx.ch>
2018-08-25 15:49:43 +0200

    Adapt voltage to measured voltage.

--- lib/light_resistor/light_resistor.h
+++ lib/light_resistor/light_resistor.h
@@ -3,7 +3,7 @@
 #define LIGHTSENSOR_PIN A0
 //#define INPUT_VOLTAGE 504 // 5.04 volt on the uno multiplied by ten since map() works with long
 #define INPUT_VOLTAGE 0 // define voltage read from the board.
-#define REFERENCE_VOLTAGE 500 // 5.0 volt exected
+#define REFERENCE_VOLTAGE 489 // 5.0 volt exected
 
 extern int inputVoltage;
 extern int lightIntensity; 
