b599dd51a1abce49ea9c05ad07316c87ea07e9ff
Martyn Messerli <realloser@gmx.ch>
2018-08-25 14:14:44 +0200

    Move code to mqtt lib

--- src/main.cpp
+++ src/main.cpp
@@ -13,63 +13,12 @@
 #include <receive_data.h>
 #include <delayAsync.h>
 
-#include <SPI.h>
 #include <Ethernet.h>
-#include <PubSubClient.h>
-
-#define MQTT_CLIENT_ID "UNO_CLIENT"
-
-// Update these with values suitable for your network.
+#include <mqtt.h>
 byte mac[] = {0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED};
 IPAddress ip(192, 168, 1, 123);
 IPAddress server(192, 168, 1, 169);
-#include <PubSubClient.h>
-EthernetClient ethClient;
-PubSubClient client(ethClient);
 
-bool reconnectMQTT()
-{
-  // Loop until we're reconnected
-  while (!client.connected())
-  {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect(MQTT_CLIENT_ID))
-    {
-      Serial.println("connected");
-      return true;
-    }
-    else
-    {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-      return false;
-    }
-  }
-  return true;
-}
-
-void publishMQTT(char *message)
-{
-  if (reconnectMQTT())
-  {
-    Serial.print("Publish message: ");
-    Serial.println(message);
-
-    client.publish("M18/Environment", message);
-  }
-}
-
-void setupMQTT()
-{
-  client.setServer(server, 1883);
-  Ethernet.begin(mac, ip);
-  // Allow the hardware to sort itself out
-  delay(1500);
-}
 
 void setup()
 {
